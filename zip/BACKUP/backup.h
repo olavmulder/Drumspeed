2520e785c972ce0be5f6f12954aea97dfcbb794b
olavmulder <60880343+olavmulder@users.noreply.github.com>
2021-12-10 16:18:42 +0100

    Delete zip directory

--- zip/BACKUP/backup.h
+++
@@ -1,6 +0,0 @@
-#ifndef BACKUP_H
-#define BACKUP_H
-#include "../LIST/linkedList.h"
-unsigned int BackupData(float *speed, float*rpm, float* amount, char* time);
-void BackupGetAllData(float**array, char*timeArray, int sizeLinkedList);
-#endif
