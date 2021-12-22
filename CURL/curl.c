421ee4076cc041d82b1150ef4a48b4110bca721f
olavmulder <olavmulder02@gmail.com>
2021-12-22 11:32:39 +0100

    simplefied curl code

--- CURL/curl.c
+++ CURL/curl.c
@@ -1,61 +1,39 @@
 #include "curlData.h"
+const size_t standLenght = 5*8;
 int SendDataToDatabase(float *_speed, float*_rpm, char *ipAddr, float* _amountToRead, char* time, char backup)
 {
 
-    int i=0;
-    
+    //get lenght speed
     size_t lenData = (size_t)snprintf(NULL, 0, "%.4f", *_speed);
     char *speed = (char*)malloc(lenData);
     snprintf(speed, lenData+1, "%.6f", *_speed);
-
+    //get lenght rpm
     lenData = (size_t)snprintf(NULL, 0, "%.4f", *_rpm);
     char *rpm = (char*)malloc(lenData);
     snprintf(rpm, lenData+1, "%.4f", *_rpm);
-
+    //get lenght amount
     lenData = (size_t)snprintf(NULL,0, "%.4f", *_amountToRead);
     char *amountToRead = (char*)malloc(lenData);
     snprintf(amountToRead, lenData+1,"%.4f", *_amountToRead);
-
-    size_t length = strlen(standaardInfoPart1)+strlen(standaardInfoPart2)+ \
-        strlen(standaardInfoPart3)+strlen(standaardInfoPart4)+ \
-        strlen(standaardInfoPart5)+ strlen(standaardInfoPart6) + strlen(time) + strlen(amountToRead)+ \
-        strlen(rpm)+strlen(speed)+1;
+    //get lenght whole string
+    size_t length = standLenght +  strlen(ipAddr) + strlen(speed) \
+        + strlen(rpm) + strlen(amountToRead)+2;
+    //printf("backup: %c\n", backup);
     char *finleString;
-    finleString = (char*)malloc(length+1);
-    
-    for(i=0;i<(int)length+1;i++){
-        finleString[i] = '\0';
-    }
-
-    strcat(finleString,standaardInfoPart1);
-    strcat(finleString,speed);
-    strcat(finleString, standaardInfoPart2);
-    strcat(finleString,rpm);
-    strcat(finleString, standaardInfoPart3);
-    strcat(finleString,amountToRead);
-    strcat(finleString,standaardInfoPart4);
-    strcat(finleString, time);
-    strcat(finleString, standaardInfoPart5);
-    strcat(finleString, &backup);
-    strcat(finleString, standaardInfoPart6);
-    
-    free(speed);
-    free(rpm);
-    free(amountToRead);
+    finleString = (char*)malloc(length);
+    sprintf(finleString, "%s%s%.4f%s%.4f%s%.4f%s%s%c", ipAddr, standaardInfoPart1, *_speed, standaardInfoPart2, *_rpm, \
+        standaardInfoPart3,*_amountToRead,standaardInfoPart4, standaardInfoPart5, backup);
+    //printf("%s\n", finleString);
+    time = time;
    
-    lenData = (size_t)snprintf(NULL,0, "%s", ipAddr);
-    char *ipAddress = (char*)malloc(lenData+1);
-    for(i=0;i<(int)lenData+1;i++){
-        ipAddress[i] = '\0';
-    }
-    strcat(ipAddress, ipAddr);
+
     CURL *curl;
     CURLcode res;
-    
     curl = curl_easy_init();
     if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, ipAddress);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, finleString);
+        
+        curl_easy_setopt(curl, CURLOPT_URL, finleString);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
         res = curl_easy_perform(curl);
         if(res != CURLE_OK){
             free(finleString);
@@ -66,7 +44,6 @@ int SendDataToDatabase(float *_speed, float*_rpm, char *ipAddr, float* _amountTo
         curl_easy_cleanup(curl);   
     }
     free(finleString);
-    free(ipAddress);
     curl_global_cleanup();
     return 0;
 }
--- CURL/curlData.h
+++ CURL/curlData.h
@@ -14,11 +14,20 @@ struct string{
 int SendDataToDatabase(float* _speed, float* _rpm, char * ipAddr, \
  float* _amountToRead, char* _time, char backup);
 
+//static const char standaardInfoPart0[] = "?api_key=PUWI7VQCMC8BOSIT";
+static const char standaardInfoPart1[] = "&field1=";
+static const char standaardInfoPart2[] = "&field2=";
+static const char standaardInfoPart3[] = "&field3=";
+static const char standaardInfoPart4[] = "&field4=";
+static const char standaardInfoPart5[] = "&field5=";
 
+/*
 static const char standaardInfoPart1[] = "speed=";
 static const char standaardInfoPart2[] = "&rpm=";
 static const char standaardInfoPart3[] = "&amount=";
 static const char standaardInfoPart4[] = "&measurementTime=";
 static const char standaardInfoPart5[] = "&backup=";
 static const char standaardInfoPart6[] = "&username=olav&pwd=Ammeraal123!&database=testTable";
+*/
+
 #endif
